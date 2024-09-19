e43fd53b32be24021952a3d1682c48b2b472c44c
Luke Howard <810363519+L-Howard@users.noreply.github.com>
2024-09-19 13:00:50 -0700

    Finished implementing resize button grid stays the same size and boxes resize to fit container

--- JavaScript.js
+++ JavaScript.js
@@ -8,13 +8,17 @@ function createRows (rows) {
         const divrow = container.appendChild(document.createElement("div"));
         i2 = 0;
         divrow.classList.add("divrow");
+        divrow.style.flexShrink = "1";
         while(i2 < rows){
 
         
         const div = divrow.appendChild(document.createElement("div"));
         div.classList.add("box");
-        div.style.padding = "20px";
+        const boxSize = Math.floor(500 / rows);
+        div.style.height = `${boxSize}px`;
+        div.style.width = `${boxSize}px`;
         div.style.border = "1px solid black";
+
 
         i2 ++;
     }
@@ -38,6 +42,14 @@ const button = document.querySelector("button")
 
 button.addEventListener("click", function (){
     size = prompt("How many squares per side? Max: 100");
+
+    if(size > 100 || size <= 0) {
+        alert("Invalid size");
+    
+    }
+    else {
+
+    
     allRows = document.querySelectorAll(".divrow")
     allRows.forEach(row => {
         row.remove()
@@ -46,5 +58,5 @@ button.addEventListener("click", function (){
     
     
     createRows(size);
-
+    }
 })
